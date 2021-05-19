e561394468366f3534fbfbd93fd8aeab9f84257b
ricardo <rarrosr@gmail.com>
2021-05-19 00:45:10 -0400

    React-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import {Container} from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header/>
+
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+        
+      </main>
+
+      <Footer/>
+      
     </>
   );
 }
