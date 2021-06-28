198db47ff1c2cca5bb62d684ccc086a1b77564ef
ricardo <rarrosr@gmail.com>
2021-06-28 00:50:25 -0400

    added react router & replaced links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,37 +1,47 @@
-import React from 'react'
-import {Container, Navbar, Nav} from 'react-bootstrap';
+import React from 'react';
+
+//
+import { LinkContainer } from 'react-router-bootstrap';
+
+//
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
-
   return (
-
     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          {/*  */}
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <Navbar.Collapse id="basic-navbar-nav" className="flex justify-content-end">
-
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="flex justify-content-end"
+          >
             <Nav className="">
-              <Nav.Link href="/cart">
-                <i className='fas fa-shopping-cart'></i>
-                Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className='fas fa-user'></i>
-                Sign In
-              </Nav.Link>          
+              <LinkContainer to="/cart">
+                <Nav.Link active={false}>
+                  <i className="fas fa-shopping-cart"></i>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link active={false}>
+                  <i className="fas fa-user"></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
-      
           </Navbar.Collapse>
         </Container>
-
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProductScreen = (props) => {
+  return <div>Products</div>;
+};
+
+ProductScreen.propTypes = {};
+
+export default ProductScreen;
