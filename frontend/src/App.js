f34de1748ee28a5e5be80d78948a2e0d272ddb50
ricardo <rarrosr@gmail.com>
2021-06-28 00:19:21 -0400

    added Rating component and router installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+//
 import Footer from './components/Footer';
 import Header from './components/Header';
-
-import { Container } from 'react-bootstrap';
-
 import HomeScreen from './screens/HomeScreen';
+
+//
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
